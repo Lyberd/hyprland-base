d16431c391f0abfd37bf8da6102a5a9bc4343b7b
Lyberd <aleksandrlok21@gmail.com>
2026-01-05 23:02:46 +0300

    Add files via upload

---
+++ base/install-packages.sh
@@ -0,0 +1,3 @@
+sudo pacman -Syu
+
+sudo pacman -S git firefox okular brightnessctl ttf-jetbrains-mono-nerd nwg-look papirus-icon-theme thunar hyprlock zip unzip waybar wofi neovim clang ttf-font-awesome otf-font-awesome ttf-jetbrains-mono base-devel
