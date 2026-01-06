f4d54e5a7ff7e0af4f5c5338ef1757e49b0f67b8
Lyberd <aleksandrlok21@gmail.com>
2026-01-06 19:48:35 +0300

    Update install-packages.sh

--- base/install-packages.sh
+++ base/install-packages.sh
@@ -1,3 +1,3 @@
 sudo pacman -Syu
 
-sudo pacman -S git firefox okular brightnessctl ttf-jetbrains-mono-nerd nwg-look papirus-icon-theme thunar hyprlock zip unzip waybar wofi neovim clang ttf-font-awesome otf-font-awesome ttf-jetbrains-mono base-devel swaybg
+sudo pacman -S --needed git firefox okular brightnessctl ttf-jetbrains-mono-nerd nwg-look papirus-icon-theme thunar hyprlock zip unzip waybar wofi neovim clang ttf-font-awesome otf-font-awesome ttf-jetbrains-mono base-devel swaybg
